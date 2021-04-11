47e7cf4061852848e2e16813f95efd393a5dac24
Roni <roni.c.castro@gmail.com>
2021-04-11 18:44:35 -0300

    feat: createResource to allow generic fetch

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,20 +4,34 @@
 import * as React from 'react'
 import {PokemonDataView, PokemonErrorBoundary, fetchPokemon} from '../pokemon'
 
-let pokemon, error
-const pokemonPromise = fetchPokemon('pikacha').then(
-  data => (pokemon = data),
-  err => (error = err),
-)
+function createResource(promise) {
+  let status = 'pending',
+    data,
+    error
+  let resultPromise = promise.then(
+    resolved => {
+      status = 'resolved'
+      data = resolved
+    },
+    rejected => {
+      status = 'rejected'
+      error = rejected
+    },
+  )
+  return {
+    read() {
+      if (status === 'pending') throw resultPromise
+      if (status === 'rejected') throw error
+      if (status === 'resolved') return data
+      throw new Error('This state should be impossible')
+    },
+  }
+}
+
+const pokemonResource = createResource(fetchPokemon('pikachu'))
 
 function PokemonInfo() {
-  if (error) {
-    throw error
-  }
-  if (!pokemon) {
-    throw pokemonPromise
-  }
-
+  const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
